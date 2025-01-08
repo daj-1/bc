7326171472d866e1be0cec29b2a389991a49d668
SKornejeva <162116289+SKornejeva@users.noreply.github.com>
2025-01-08 09:11:02 +0200

    Update models.py

--- server/models.py
+++ server/models.py
@@ -10,3 +10,6 @@ class Book(db.Model):
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=True)
     copies = db.Column(db.Integer, nullable=False)
+
+def __repr__(self):
+        return f"<Book {self.title}>"
